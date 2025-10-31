0e1a66c393aeb37ec08c433a0880e1f22b355596
ASPulido <ariesoripuli@yahoo.com.ph>
2025-10-31 17:23:02 +0800

    shop queries

--- public/shop/index.php
+++ public/shop/index.php
@@ -7,15 +7,51 @@
     $db = new DB($pdo);
 
     // Product Categories
-    $sql = "SELECT value, description FROM universal_lookup WHERE category = 'PRODUCT CATEGORY' ORDER BY description ";
-    $productCategorySet = $db->fetch($sql, [], [], 'all');
+    $sqlCat = "SELECT ul.value, ul.description, COUNT(sp.id) AS total_rows
+        FROM universal_lookup ul
+        LEFT JOIN shop_products sp ON sp.category_id = ul.value
+        WHERE ul.category = 'PRODUCT CATEGORY'
+        GROUP BY ul.value, ul.description
+        ORDER BY ul.description ";
+    $productCategorySet = $db->fetch($sqlCat, [], [], 'all');
 
-    // Products
-    $sql = "SELECT shop_products.*, universal_lookup.description as category 
-            FROM shop_products 
-            INNER JOIN universal_lookup ON universal_lookup.value = shop_products.category_id
-            WHERE universal_lookup.category = 'PRODUCT CATEGORY'";
+
+    $categoryId = 0;
+
+    if ((isset($_GET['category_id'])) OR (isset($_POST['submit']))) {
+
+        if (isset($_GET['category_id'])) {
+            $categoryId = $_GET['category_id'];
+            // Products
+            $sql = "SELECT shop_products.*, universal_lookup.description as category 
+                    FROM shop_products 
+                    INNER JOIN universal_lookup ON universal_lookup.value = shop_products.category_id
+                    WHERE universal_lookup.category = 'PRODUCT CATEGORY' ";
+                    if ($categoryId > 0) {
+                        $sql .= " AND category_id = $categoryId ";
+                    }
+        }
+
+        if (isset($_POST['submit'])) {
+            $searchKey = $_POST['search'];
+            // Products
+            $sql = "SELECT shop_products.*, universal_lookup.description as category 
+                    FROM shop_products 
+                    INNER JOIN universal_lookup ON universal_lookup.value = shop_products.category_id
+                    WHERE universal_lookup.category = 'PRODUCT CATEGORY' AND
+                        shop_products.product_name LIKE '%" . trim($searchKey) . "%'";
+        }
+
+    } else {
+        // Products
+        $sql = "SELECT shop_products.*, universal_lookup.description as category 
+                FROM shop_products 
+                INNER JOIN universal_lookup ON universal_lookup.value = shop_products.category_id
+                WHERE universal_lookup.category = 'PRODUCT CATEGORY' ";
+    }
+
     $productSet = $db->fetch($sql, [], [], 'all');
+    
 
     // SEO
     include SHOP_META;
@@ -32,7 +68,7 @@
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-6 col-12">
                     <div class="breadcrumbs-content">
-                        <h1 class="page-title">Shop Grid</h1>
+                        <h1 class="page-title">Shop</h1>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-12">
@@ -57,9 +93,9 @@
                         <!-- Start Single Widget -->
                         <div class="single-widget search">
                             <h3>Search Product</h3>
-                            <form action="#">
-                                <input type="text" placeholder="Search Here...">
-                                <button type="submit"><i class="lni lni-search-alt"></i></button>
+                            <form method="post" action="<?php $_PHP_SELF ?>">
+                                <input type="text" name='search' placeholder="Search Here...">
+                                <button type="submit" name='submit'><i class="lni lni-search-alt"></i></button>
                             </form>
                         </div>
                         <!-- End Single Widget -->
@@ -68,9 +104,13 @@
                             <h3>All Categories</h3>
                             <ul class="list">
                                 <?php 
+                                    echo ("<li>");
+                                        echo ("<a href='" . SHOP_URL . "/index.php?category_id=0'>Show All</a>");
+                                    echo ("</li>");
                                     foreach ($productCategorySet as $row) {
+                                        
                                         echo ("<li>");
-                                            echo ("<a href='product-grids.html'>".$row['description']."</a><span>(18)</span>");
+                                            echo ("<a href='" . SHOP_URL . "/index.php?category_id=$row[value]'>" . $row['description']."</a><span>&nbsp;(".$row['total_rows'].")</span>");
                                         echo ("</li>");
                                     }
                                 ?>
@@ -85,7 +125,7 @@
                         <div class="product-grid-topbar">
                             <div class="row align-items-center">
                                 <div class="col-lg-7 col-md-8 col-12">
-                                    <div class="product-sorting">
+                                    <!-- <div class="product-sorting">
                                         <label for="sorting">Sort by:</label>
                                         <select class="form-control" id="sorting">
                                             <option>Popularity</option>
@@ -96,7 +136,7 @@
                                             <option>Z - A Order</option>
                                         </select>
                                         <h3 class="total-show-product">Showing: <span>1 - 12 items</span></h3>
-                                    </div>
+                                    </div> -->
                                 </div>
                                 <div class="col-lg-5 col-md-4 col-12">
                                     <nav>
@@ -125,7 +165,7 @@
                                                         $image = "/resize.php?file=".$row['image']."&w=335&h=335";
                                                         echo ("<img src='". SHOP_IMAGES . $image. "' alt='#'>");
                                                         echo ("<div class='button'>");
-                                                            echo ("<a href='product-details.php?id=$row[id]' class='btn'><i class='lni lni-cart'></i> Add to Cart</a>");
+                                                            echo ("<a href='product-details.php?id=$row[id]' class='btn'><i class='lni lni-eye'></i> Details</a>");
                                                         echo ("</div>");
                                                     echo ("</div>");
                                                     echo ("<div class='product-info'>");
@@ -163,7 +203,7 @@
                                                                 $image = "/resize.php?file=".$row['image']."&w=335&h=335";
                                                                 echo ("<img src='". SHOP_IMAGES . $image. "' alt='#'>");
                                                                 echo ("<div class='button'>");
-                                                                    echo ("<a href='product-details.php?id=$row[id]' class='btn'><i class='lni lni-cart'></i> Add to Cart</a>");
+                                                                    echo ("<a href='product-details.php?id=$row[id]' class='btn'><i class='lni lni-eye'></i> Details</a>");
                                                                 echo ("</div>");
                                                             echo ("</div>");
                                                         echo ("</div>");
